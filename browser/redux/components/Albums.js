550f7ff8ec921ee8aa33b2016f2b14d166142cb8
Colten Jackson <Colten Jackson>
2016-10-12 11:22:52 -0400

    mapped albums

--- browser/redux/components/Albums.js
+++ browser/redux/components/Albums.js
@@ -17,28 +17,21 @@ class Albums extends Component
              <div>
                 <h3>Albums</h3>
                 <div className="row">
-                    <div className="col-xs-4">
-                    <a className="thumbnail" href="#">
-                        <img src="http://placeholdit.imgix.net/~text?txtsize=33&txt=ALBUMoneIMAGE&w=300&h=300" />
-                        <div className="caption">
-                        <h5>
-                            <span>ALBUM ONE NAME HERE</span>
-                        </h5>
-                        <small>NUMBER OF SONGS HERE songs</small>
-                        </div>
-                    </a>
-                    </div>
-                    <div className="col-xs-4">
-                    <a className="thumbnail" href="#">
-                        <img src="http://placeholdit.imgix.net/~text?txtsize=33&txt=ALBUMtwoIMAGE&w=300&h=300" />
-                        <div className="caption">
-                        <h5>
-                            <span>ALBUM TWO NAME HERE</span>
-                        </h5>
-                        <small>NUMBER OF SONGS HERE songs</small>
-                        </div>
-                    </a>
-                    </div>
+                    {
+                        this.props.albumList.map( anAlbum => (
+                            <div className="col-xs-4">
+                            <a className="thumbnail" href="#">
+                                <img src= { anAlbum.imageUrl } />
+                                <div className="caption">
+                                <h5>
+                                    <span> {anAlbum.name} </span>
+                                </h5>
+                                <small> {anAlbum.songs.length} </small>
+                                </div>
+                            </a>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
              )
