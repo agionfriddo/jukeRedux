db43cbbd8aada551f696057d3c3a5a722c1f275d
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2016-10-12 13:00:06 -0400

    refactored the reducer into root reducer and property reducers

--- browser/react/ourRedux.js
+++ browser/react/ourRedux.js
@@ -1,6 +1,6 @@
 import initialState from './initialState.js'
 import {createStore} from 'redux'
-import {applyMiddleware} from 'redux'
+import {applyMiddleware, combineReducers} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 // console.log("CREATELOGGER", createLogger)
@@ -11,16 +11,29 @@ export const RECEIVE_ALBUMS = 'RECEIVE_ALBUMS';
 
 // receives action argument from store.dispatch it also receives state from
 // dispatch as well
-function reducer(state = initialState, action) {
+
+
+
+
+// each individual reducer works with a property on the store. It returns the expected
+// data type, which it then passes to the rootReducer below.
+function albumListReducer(state = [], action) {
   switch (action.type) {
     case "RECEIVE_ALBUMS":
-      return Object.assign({}, state, {albumList: action.albumsArr})
+      return action.albumsArr
     default:
       return state;
   }
 }
 
-let store = createStore(reducer, myMiddle);
+// The root reducer takes in data from the other reducers and then assigns the
+// properties that will be on the store. It is then passed to createStore to make
+// the new state.
+let rootReducer = combineReducers({
+  albumList: albumListReducer
+});
+
+let store = createStore(rootReducer, myMiddle);
 
 // store.dispatch hands its argument to the reducer function store is aware of
 // state because it receieves reducer as an argument when it is created
