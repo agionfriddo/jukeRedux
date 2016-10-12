210ecb3f58c58092bc45523701ad1bf64d30fd20
Colten Jackson <Colten Jackson>
2016-10-12 12:40:17 -0400

    only a little more beautiful

--- browser/react/ourRedux.js
+++ browser/react/ourRedux.js
@@ -1,6 +1,6 @@
 import initialState from './initialState.js'
-import { createStore } from 'redux'
-import { applyMiddleware } from 'redux'
+import {createStore} from 'redux'
+import {applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 // console.log("CREATELOGGER", createLogger)
@@ -9,25 +9,20 @@ var myMiddle = applyMiddleware(createLogger(), thunkMiddleware)
 
 export const RECEIVE_ALBUMS = 'RECEIVE_ALBUMS';
 
-
-// receives action argument from store.dispatch
-// it also receives state from dispatch as well
-function reducer (state = initialState, action){
-    switch (action.type){
-        case "RECEIVE_ALBUMS": return Object.assign(
-            {},
-            state,
-            {albumList: action.albumsArr}
-        )
-        default: return state;
-    }
+// receives action argument from store.dispatch it also receives state from
+// dispatch as well
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "RECEIVE_ALBUMS":
+      return Object.assign({}, state, {albumList: action.albumsArr})
+    default:
+      return state;
+  }
 }
 
 let store = createStore(reducer, myMiddle);
 
-
-// store.dispatch hands its argument to the reducer function
-// store is aware of state because it receieves reducer as an argument
-// when it is created
+// store.dispatch hands its argument to the reducer function store is aware of
+// state because it receieves reducer as an argument when it is created
 
 export default store;
--- browser/redux/components/AlbumsContainer.js
+++ browser/redux/components/AlbumsContainer.js
@@ -4,20 +4,20 @@ import {RECEIVE_ALBUMS} from '../../react/ourRedux.js'
 import {fetchAlbumsFromServer} from '../ActionCreators'
 
 const mapStateToProps = function (state, ownProps) {
-    return {albumList: state.albumList};
+  return {albumList: state.albumList};
 };
 
 const mapDispatchToProps = function (dispatch, ownProps) {
-    //This returns an object. loadAlbums is a method of the object.
-    return {
-        loadAlbums(albums) {
-            // fetchAlbums invoked, returns a function THUNK recognizes that dispatch has
-            // been passed a function instead of a Action Obj, so it says, ' i better handle
-            // this async shit' THUNK should handle passing the dispatch func to fetchAlbums
-            // so that dispatch can be called when its time comes
-            dispatch(fetchAlbumsFromServer());
-        }
+  //This returns an object. loadAlbums is a method of the object.
+  return {
+    loadAlbums(albums) {
+      // fetchAlbums invoked, returns a function THUNK recognizes that dispatch has
+      // been passed a function instead of a Action Obj, so it says, ' i better handle
+      // this async shit' THUNK should handle passing the dispatch func to
+      // fetchAlbums so that dispatch can be called when its time comes
+      dispatch(fetchAlbumsFromServer());
     }
+  }
 }
 
 const AlbumsContainer = connect(mapStateToProps, mapDispatchToProps)(Albums);
